56382e28df6f403595c476cb8c7b200825d5e886
Christopher Woods <Christopher.Woods@LibertyMutual.com>
2017-06-12 15:56:37 -0400

    Add guess notification (success / fail)

--- assets/javascripts/javascript.js
+++ assets/javascripts/javascript.js
@@ -17,14 +17,16 @@
   var cat6 = $("#row6 h3");
   var categories = [cat1, cat2, cat3, cat4, cat5, cat6];
   var storedCats = [];
+  var alert = $('.alert_container');
+
 
   // Getting a random int
   function getRandomInt(min, max) {
       return Math.floor(Math.random() * (max - min + 1)) + min
   }
 
-  // Load a random set of 6 categories from jeopardy API
-  // using the random int function for your offset
+  // Load a random set of 6 categories from jeopardy API using the
+  // random int function for your offset and store them into an array
   $(function(){
 
     $.get( "http://jservice.io/api/categories?count=6&offset=" + getRandomInt(1, 17000), function( data ) {
@@ -32,24 +34,18 @@
       for (var i = 0; i < data.length; i++) {
         categories[i].html(data[i].title);
         storedCats.push(data[i].id);
-        // console.log(data[i].id + ' api');
-        // console.log(storedCats[i] + ' stored values');
       }
 
     });
 
-    // var categories = [cat1, cat2, cat3, cat4, cat5, cat6];
-
   });
 
+  // Pull random question according to the category of the row
   button.click(function(){
 
     buttonValue = $(this).text();
     var buttonCatNum = $(this).val();
     $("#answerInput").val('');
-    // console.log(storedCats[buttonCatNum]);
-
-    // console.log(buttonValue + ' button value');
 
     // *************************************
     // Need to modify the get query with promises & an if statement
@@ -65,9 +61,7 @@
       showQuestion.css("display","block");
       question.html(data[0].question + ".");
 
-      // console.log(data[0].category_id + " : this is the category ID of the question we pulled");
       console.log(goodAnswer);
-      // console.log(answerScore);
 
     });
 
@@ -84,7 +78,15 @@
     if (userAnswer == goodAnswer) {
       userScore = userScore + answerScore
       $("#scoreContainer #score").html(userScore);
-
+      alert.append('<div class="alert alert-success alert-dismissible" role="alert">' +
+      '<button type="button" class="close" data-dismiss="alert" aria-label="Close"> ' +
+      '<span aria-hidden="true">&times;</span></button>' +
+      '<strong>Success!</strong> Your answer was correct. ' + answerScore + ' : has been added to your score!</div>');
+    } else {
+      alert.append('<div class="alert alert-danger alert-dismissible" role="alert"> ' +
+      '<button type="button" class="close" data-dismiss="alert" aria-label="Close"> ' +
+      '<span aria-hidden="true">&times;</span></button>' +
+      '<strong>Warning!</strong> Your answer was incorrect. The correct answer is: ' + goodAnswer + '. No points awarded!</div>')
     }
 
     $(this).prop('disabled', true);
